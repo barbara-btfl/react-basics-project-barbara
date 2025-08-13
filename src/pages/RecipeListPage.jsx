2ddff1796e46f86991157db1c4f93a52c3f5b990
BARBARA PETERS <barbara.peters@gmail.com>
2025-08-13 07:43:15 +0200

    removed useless box

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -12,24 +12,22 @@ export const RecipeListPage = ({ onSelect }) => {
   return (
     <Center>
       <Box padding={4}>
-        <Box>
-          {/* Filter the recipes based on the searchField */}
-          {/* Laat elke receptkaart verwijzen naar een RecipePage met key={label} */}
-          <Box padding={4} textAlign="center" mb={8}>
-            <Heading marginBottom={"0.5em"}>Your Recipe App</Heading>
-            <SearchBox watErGaatKomen={setSearchField} />
-          </Box>
-          <Wrap spacing="2rem" justify="center">
-            {searchField.map((hit) => (
-              <WrapItem key={hit.recipe.label}>
-                <RecipeCard
-                  recipe={hit.recipe}
-                  onClick={() => onSelect(hit.recipe.label)}
-                />
-              </WrapItem>
-            ))}
-          </Wrap>
+        {/* Filter the recipes based on the searchField */}
+        {/* Laat elke receptkaart verwijzen naar een RecipePage met key={label} */}
+        <Box padding={4} textAlign="center" mb={8}>
+          <Heading marginBottom={"0.5em"}>Your Recipe App</Heading>
+          <SearchBox watErGaatKomen={setSearchField} />
         </Box>
+        <Wrap spacing="2rem" justify="center">
+          {searchField.map((hit) => (
+            <WrapItem key={hit.recipe.label}>
+              <RecipeCard
+                recipe={hit.recipe}
+                onClick={() => onSelect(hit.recipe.label)}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
       </Box>
     </Center>
   );
